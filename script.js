77790c54ef3e5133988321a39e353a0d772ae91e
AsiaKabacinska <75363286+AsiaKabacinska@users.noreply.github.com>
2025-05-06 11:40:38 +0200

    changes in styles, grid added and flexbox

--- script.js
+++ script.js
@@ -21,3 +21,19 @@ function hello() {
 }
 console.log(hello());
 
+if ('JavaSrcipt!="Java') {
+  console.log("Java to nie JS");
+}
+if (2 < 4) {
+  console.log("2 jest mniejsze od 4");
+}
+if ("2" === 2) {
+  console.log("to jest prawda ");
+}
+
+const button = document.querySelector(".page-header__button--js");
+
+button.addEventListener("click", (e) => {
+  const header = document.querySelector(".page-header__button--js");
+  header.innerHTML = "klik,klik!";
+});
